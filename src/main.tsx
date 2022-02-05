9ef11e3c910f2ea66830d840baf5ff6c480d1284
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-05 08:28:07 -0500

    cartprovider

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import CartProvider from "../src/context/ProviderCart";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CartProvider>
+      <App />
+    </CartProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
