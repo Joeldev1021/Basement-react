1808983b4b8f7be4668bd48eb4acce00beaaea22
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-05 08:30:43 -0500

    Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import ModalCart from "./components/modal/ModalCart";
 import Home from "./pages/Home";
 
 function App () {
   return (
     <div className="App">
       <Home/>
-      <ModalCart/>
     </div>
   );
 }
