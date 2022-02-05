935e18e2f85c8ebcc29821f26e9ceb840138cdd6
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-05 08:31:08 -0500

    styles and header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,20 @@
-import BtnCart from "../cart/BtnCart";
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import BtnCart from "../BtnCart/BtnCart";
+import ModalCart from "../modal/ModalCart";
 import "./header.css";
 
 const Header = () => {
+  const [showModal, setShowModal] = useState(false);
+  const handleModal = () => {
+    setShowModal(!showModal);
+  };
+
   return (
       <header className='header'>
           <h1 className="header-title">Basement</h1>
-         <BtnCart/>
+         <BtnCart handleModal={handleModal}/>
+         {showModal && <ModalCart handleModal={handleModal}/>}
      </header>
   );
 };
