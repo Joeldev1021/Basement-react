67df101ecef592dce229e186cab60bc6bbc82bab
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-06 08:32:49 -0500

    item info price and name

--- src/components/articles/Articles.tsx
+++ src/components/articles/Articles.tsx
@@ -2,13 +2,18 @@ import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
 import "./styles.css";
 const Articles = () => {
-  const { item } = useContext(CartContext);
+  const { items, addItemToCart } = useContext(CartContext);
+
   return (
       <div className="container-grid">
-        {item?.map(item => {
+        {items?.map(item => {
           return (
-              <div className="grid-item" key={item.id}>
+              <div onClick={() => addItemToCart(item)} className="grid-item" key={item.id}>
                 <img src={item.img} alt={item.name} />
+                 <div className="item-info">
+                   <p>{item.name}</p>
+                   <p>${item.price}</p>
+                 </div>
               </div>
           );
         })}
