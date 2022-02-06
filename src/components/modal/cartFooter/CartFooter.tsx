7eba2bc6e69bfb69b2ab0371e31620965f11167c
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-06 08:38:16 -0500

    sum price article

--- src/components/modal/cartFooter/CartFooter.tsx
+++ src/components/modal/cartFooter/CartFooter.tsx
@@ -1,8 +1,18 @@
+/* eslint-disable no-unused-vars */
+import { useContext } from "react";
+import { CartContext } from "../../../context/CartContext";
+import { ItemArticle } from "../../../type";
 import "./styles.css";
+
 const CartFooter = () => {
+  const { itemCart } = useContext(CartContext);
+  const reduce = (accumulator: number, currentValue: ItemArticle) => accumulator + (currentValue.count * currentValue.price);
+
+  const result = itemCart.reduce(reduce, 0);
+
   return (
     <footer className="cart-footer">
-      <p>TOTAL: $37,50</p>
+      <p>TOTAL: ${result}</p>
       <button>CHECKOUT</button>
     </footer>
   );
