28dad651d60d95bd1221b2bd8416469bcfdf0a74
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-07 12:15:39 -0500

    type cartItem

--- src/type/index.ts
+++ src/type/index.ts
@@ -5,3 +5,11 @@ export interface ItemArticle {
     img: string,
     count?: number
 };
+
+export interface CartItem {
+    id: number,
+    name: string,
+    price: number,
+    img: string,
+    count: number
+}
