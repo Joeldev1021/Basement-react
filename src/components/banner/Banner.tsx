ee247c51422ad88ea026f2e6c9d7efdac8fb7ae4
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-08 10:38:28 -0500

    add marquee component

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,10 +1,11 @@
+import Marquee from "react-fast-marquee";
 import "./style.css";
 const Banner = () => {
   return (
     <div className="banner">
       <img src="./header.svg" alt="banner" />
       <div className="banner-text">
-          <marquee>A man cant have enough basement swag</marquee>
+          <Marquee gradient={false}>A man cant have enough basement swag</Marquee>
       </div>
     </div>
   );
